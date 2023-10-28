5a5be440f169daf515a2d543ca952a13f0f76cf6
unknown <Poojabhavsar036@gmail.com>
2023-10-28 17:33:54 +0530

    chainlit.py ready

---
+++ chainlit.py
@@ -0,0 +1,19 @@
+import os
+from langchain import PromptTemplate, OpenAI, LLMChain
+import chainlit as cl
+from constants import openai_key
+
+os.environ["OPENAI_API_KEY"] = openai_key
+
+template = """Question: {question}
+
+Answer = Lets think step-by-step.""" 
+
+@cl.langchain_factory(use_async=True)
+def factory():
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+    
+    return llm_chain
+
+    
